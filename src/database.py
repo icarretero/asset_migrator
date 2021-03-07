3c3a3c5f3167a3b9729c33c90abe7623c8455d30
Nacho Pérez Carretero <ignacio.perez@udarealestate.com>
2021-03-07 20:56:25 +0100

    Database: create iterator, all_jobs method and fix connection issues

--- src/database.py
+++ src/database.py
@@ -9,12 +9,14 @@ class DataBaseConfig:
     user: str
     password: str
     database: str
-    host: str = "localhost"
+    host: str = "127.0.0.1"
     port: str = 3306
 
 class DataBase:
+
     def __init__(self, dbconfig):
         self.dbconfig = dbconfig
+        self.conn = self._get_connection()
         self.cursor = self._get_cursor()
 
     def _get_connection(self):
@@ -28,12 +30,32 @@ class DataBase:
             )
         except mariadb.Error as e:
             raise DataBaseException(e)
+        return conn
 
     def _get_cursor(self):
-        conn = self._get_connection()
-        return conn.cursor()
+        return self.conn.cursor()
+
+    def _batch_iterator(self, batch_size=1000):
+        while True:
+            results = self.cursor.fetchmany(batch_size)
+            if not results:
+                break
+            for result in results:
+                yield result
 
     def test_database(self):
         self.cursor.execute(
             "SELECT 1;"
         )
+
+    def close(self):
+        self.conn.close()
+
+    def all_jobs(self, table, id_column, id_path, batch_size):
+        query = "SELECT {id}, {path} FROM {table}".format(
+            id=id_column,
+            path=id_path,
+            table=table
+        )
+        self.cursor.execute(query)
+        return self._batch_iterator(batch_size)
